1bfbe938f086c4656508f63a879f319f15b5a3a5
A403 <a403@KDG133s-Mac-Pro.local>
2023-01-24 16:33:00 +0900

    0124

---
+++ test.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+
+		string input = Console.ReadLine();
+		string[] input2 = Console.ReadLine().Split(" ");
+
+        int N = int.Parse(input);
+        
+        
+        
+	}
+}
